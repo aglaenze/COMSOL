a57e20cfa0631b3738a85f960505393e7af05a02
aglaenze <aude.glaenzer@cern.ch>
2020-08-24 10:36:58 +0200

    modified launch.sh

--- launch.sh
+++ launch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 rm job.sub
+rm log/*
+rm error/*
+rm output/*
 
 export LD_LIBRARY_PATH=${GARFIELD_HOME}/Install/lib:$LD_LIBRARY_PATH
 
@@ -13,8 +16,11 @@ touch job.sub
 echo 'executable      = signal' >> job.sub
 echo 'arguments       = '$hv 0 >> job.sub
 echo 'output          = output/ex.$(ClusterId).$(ProcId).out' >> job.sub
+echo 'input          = input.txt' >> job.sub
 echo 'error           = error/ex.$(ClusterId).$(ProcId).err' >> job.sub
 echo 'log             = log/ex.$(ClusterId).log' >> job.sub
+echo 'getenv=true' >> job.sub
+#echo 'environment	= LD_LIBRARY_PATH=${GARFIELD_HOME}/Install/lib:$LD_LIBRARY_PATH' >> job.sub
 #echo 'transfer_output_files   =' "$phrase" >> job.sub
 echo 'transfer_output_remaps  = "'$phraseIn'"="'$phraseOut'" ' >> job.sub
 echo 'queue' $numberOfJobs >> job.sub
