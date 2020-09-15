bbfb81ff1dc5b9b191564f02572dce29a88082b4
aglaenze <aude.glaenzer@cern.ch>
2020-09-15 16:05:18 +0200

    created bash script to rename root files after they have been processed in jobs

---
+++ renameRootFiles.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+nNotExisting=0
+i=0
+
+filePrefix=rootFiles/Ar-iC4H10/model10/fe-signal-noibf-350-430-530-830-950
+
+for ((k=0;k<300;k++)); do
+fileIn=$filePrefix-$k.root
+if ! test -f $fileIn ;
+ then
+ nNotExisting=$(( $nNotExisting + 1))
+#echo file $k does not exist
+else
+fileOut=$filePrefix-$i.root
+echo moving $fileIn to $fileOut
+mv $fileIn $fileOut
+i=$(( $i + 1))
+fi
+done
+
+echo $nNotExisting files did not exist
+
+if ! test -f $filePrefix-0.root ;
+then
+mv $filePrefix-0.root $filePrefix-$i.root
+fi
